5fb978b304ed271ffb69dba51e9766826d2b9bbb
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-06-07 07:44:31 +0530

    update monitor config for consistent scaling

--- home-manager/packages/hyprland/default.nix
+++ home-manager/packages/hyprland/default.nix
@@ -22,7 +22,10 @@ _: {
         layout = "dwindle";
       };
 
-      monitor = ["eDP-2,2560x1440@240,0x0,1.25"];
+      monitor = [
+        "eDP-1,2560x1440@240,0x0,1.25"
+        "eDP-2,2560x1440@240,0x0,1.25"
+      ];
 
       input = {
         numlock_by_default = true;
